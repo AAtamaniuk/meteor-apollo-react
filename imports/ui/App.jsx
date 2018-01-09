7fef1e58322ec16086cf2333e3bbc8bef3609fc2
Oleksandr Atamaniuk <atol.if87@gmail.com>
2018-01-09 20:34:34 +0200

    Add basic project structure and Apollo server

---
+++ imports/ui/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = (props) => {
+    return (
+        <h1>Hello!</h1>
+    );
+};
+
+App.propTypes = {};
+App.defaultProps = {};
+
+export default App;
